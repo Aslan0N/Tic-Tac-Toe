864547f90fcaa2207027b2fa89b4eca5a24f6867
Aslan0N <thenzrv@gmail.com>
2023-05-21 17:45:56 +0400

    Game

--- js/app.js
+++ js/app.js
@@ -1,70 +1,107 @@
 const playerRow = document.querySelector("#player");
 const boxes = document.querySelectorAll(".box");
-const errorBox = document.querySelector("#error")
-
-let gameOver = false;
-let winner = '';
-
-let player= 'X';
+const errorBox = document.querySelector("#error");
+let player = "X";
+let gameOver = false
+let winner;
 
 
-const startGame = ()=>{
-    playerRow.textContent = `${player}'s turn!`
-    boxes.forEach(box =>{
-        box.addEventListener('click', ()=> writtenArea(box))
-    })
-}
-startGame()
+const startGame = () => {
+  playerRow.textContent = `${player}'s turn!`;
+  boxes.forEach((box) => {
+    box.addEventListener("click", () => writtenArea(box));
+  });
+};
 
-const writtenArea=(box)=>{
-    if(box.textContent === ''){
-        box.textContent = player
-        changePlayer()
-    }else{
-        errorBox.textContent = "This box is already full !"
-        setTimeout(()=>{
-            errorBox.textContent = ''
-        }, 2000)
+const writtenArea = (box) => {
+  if (box.textContent === "") {
+    box.textContent = player;
+    changePlayer();
+  } else {
+    errorBox.textContent = "This box is already full !";
+      box.style.borderColor = "red"
+    setTimeout(() => {
+      errorBox.textContent = "";
+      box.style.borderColor = ""
+    }, 2000);
+  }
+
+  win();
+  checkTie();
+  if(gameOver){
+    playerRow.textContent = `${winner} Game is  won!`
+    boxes.forEach(block => block.style.pointerEvents = 'none');
+  }
+};
+const changePlayer = () => {
+  if (player === "X") {
+    player = "O";
+    playerRow.textContent = `${player}'s turn!`;
+  } else if (player === "O") {
+    player = "X";
+    playerRow.textContent = `${player}'s turn!`;
+  }
+};
+
+const win = () => {
+  checkRow()
+  checkCol()
+  checkDiagonal()
+};
+
+const checkRow = ()=>{
+  let row1 = boxes[0].textContent == boxes[1].textContent && 
+  boxes[0].textContent == boxes[2].textContent && boxes[0].textContent !== "";
+  let row2 = boxes[3].textContent == boxes[4].textContent &&
+  boxes[3].textContent == boxes[5].textContent && boxes[3].textContent !== ""
+  let row3 = boxes[6].textContent == boxes[7].textContent &&
+  boxes[6].textContent == boxes[8].textContent && boxes[6].textContent !== ""
+
+    if(row1 || row2 || row3){
+      gameOver= true
     }
-    
-    win()
-    if(gameOver == true){
-        playerRow.textContent = `Game is over, ${winner} Won`
-    }
-}
-const changePlayer = ()=>{
-    if(player === 'X'){
-        player = 'O'
-        playerRow.textContent = `${player}'s turn!`
-    }else if(player === 'O'){
-        player = 'X'
-        playerRow.textContent = `${player}'s turn!`
-    }
+    if(row1) return winner = boxes[0].textContent
+    if(row2) return winner = boxes[3].textContent
+    if(row3) return winner = boxes[6].textContent
 }
 
-const win = ()=>{
-    checkRows()
-    // checkColumn()
+const checkCol = ()=>{
+  let col1 = boxes[0].textContent == boxes[3].textContent &&
+  boxes[0].textContent == boxes[6].textContent && boxes[0].textContent !== ''
+  let col2 = boxes[1].textContent == boxes[4].textContent &&
+  boxes[1].textContent == boxes[7].textContent && boxes[1].textContent !== ''
+  let col3 = boxes[2].textContent == boxes[5].textContent &&
+  boxes[2].textContent == boxes[8].textContent && boxes[2].textContent !== ''
+  
+ if(col1 || col2 || col3){
+  gameOver=true
+ }
+ if(col1) return winner = boxes[0].textContent
+ if(col2) return winner = boxes[1].textContent
+ if(col3) return winner = boxes[2].textContent
 }
 
-const checkRows = ()=>{
-    // let rows1 = boxes[0].textContent == boxes[1].textContent && boxes[0].textContent == boxes[2].textContent && boxes[0] !== '';
-    // if (rows1) console.log('Restart');
-    // let rows2 = boxes[3].textContent == boxes[4].textContent && boxes[3].textContent == boxes[5].textContent && boxes[3] !== '';
-    // if (rows2) console.log('Restart');
-    let rows3 = boxes[6].textContent == boxes[7].textContent && boxes[6].textContent == boxes[8].textContent && boxes[6] !== '';
-    if (rows3) console.log('Restart');
-    if(rows3 ){
-        gameOver = true
-    }
-    if(rows3) {
-        winner = boxes[6].textContent
-    } 
-        
-    // if(rows2) return winner = boxes[3].textContent
-    // if(rows3) return winner = boxes[6].textContent
+const checkDiagonal = ()=>{
+  let diag1 = boxes[0].textContent == boxes[4].textContent && 
+  boxes[0].textContent == boxes[8].textContent && boxes[0].textContent !== ''
+  let diag2 = boxes[2].textContent == boxes[4].textContent && 
+  boxes[2].textContent == boxes[6].textContent && boxes[2].textContent !== ''
+  if(diag1 || diag2)  {
+    gameOver=true
+  }
+  if(diag1) return winner = boxes[0].textContent
+  if(diag2) return winner = boxes[2].textContent
 }
-// const checkColumn = ()=>{
-//     let column1 = boxes[0].textContent == boxes[3].textContent && boxes[0].textContent == boxes[6].textContent;
-//     if(column1) console.log('Restart game');
-// }
+
+function checkTie() {
+    // tie
+    const values = [];
+    boxes.forEach(block => values.push(block.textContent))
+    if (!values.includes("")) {
+        playerRow.textContent = "Tie !";
+        boxes.forEach(block => block.style.pointerEvents = 'none');
+    }
+}
+startGame();
+
+// ======================================================
